8b54f13179dacf02709c1da720430052338e89fa
Dhanavarsha <dhan.varsha03@gmail.com>
2018-05-02 13:20:24 +0530

    Makes fields private to the test

--- src/test/java/tests/smokeTests/TestYoutubeSearchAPI.java
+++ src/test/java/tests/smokeTests/TestYoutubeSearchAPI.java
@@ -10,25 +10,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TestYoutubeSearchAPI {
-    private static final String ENCODED_URL = "https://www.googleapis.com/youtube/v3/search";
-    private static final String API_KEY = dbReader.getKey();
-    private Map<String, String> params;
-    private Client client;
 
     @Test
     public void testSearchWithGivenQuery() throws IOException {
-        client = new Client(ENCODED_URL);
-        params = new HashMap<>();
+
+        String searchUrl = "https://www.googleapis.com/youtube/v3/search";
+        String API_KEY = dbReader.getKey();
+        Map<String, String> params = new HashMap<>();
         params.put("key", API_KEY);
         params.put("part", "snippet");
         params.put("q", "dogs");
         params.put("type", "video");
         params.put("maxResults", "10");
 
+        Client client = new Client(searchUrl);
+
         client.setRequestParameters(params);
         client.buildGetRequest();
         client.executeRequest();
-        Assert.assertEquals(client.getStatusCode(), 200);
 
+        Assert.assertEquals(client.getStatusCode(), 200);
     }
 }
